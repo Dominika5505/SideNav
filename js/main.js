6ff0060fbee241b7c8ea542391a1c17f67db07f7
Dominika Sedileková <49202790+Dominika5505@users.noreply.github.com>
2019-05-14 19:19:23 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
+const mediaQuery = window.matchMedia('(max-width: 800px)');
+
 const mouseEnterArea = document.querySelector('.mouse-enter');
+const hamburger = document.querySelector('.hamburger-menu');
+const nav = document.querySelector('.nav-wrapper');
+const container = document.querySelector('.container');
+const contentSection = document.querySelector('.content');
 
-mouseEnterArea.addEventListener('mouseenter', showNav);
+if (mediaQuery.matches) {
+  hamburger.addEventListener('click', toggleHamburger);
 
-function showNav(e) {
-  const nav = document.querySelector('.nav-wrapper');
-  nav.classList.toggle('show');
+  function toggleHamburger(e) {
+    hamburger.classList.toggle('toggle');
+    nav.classList.toggle('show');
+    contentSection.classList.toggle('push');
+  }
+
+} else {
+  mouseEnterArea.addEventListener('mouseenter', showNav);
+  
+  function showNav(e) {
+    nav.classList.toggle('show');
+  }
+
+  mouseEnterArea.addEventListener('mouseleave', hideNav);
+
+  function hideNav(e) {
+    if(nav.className.includes('show')) {
+      nav.classList.remove('show');
+    }
+  }
 }
